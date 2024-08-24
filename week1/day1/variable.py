809049cde257370e301a49ee86060a7212eab2d9
rizviahmed <rizviahmerumon@gmail.com>
2024-08-24 19:23:03 +0600

    third commit

--- week1/day1/variable.py
+++ week1/day1/variable.py
@@ -1,10 +1,62 @@
-rent=3200
-wifi=200
-food=3500
-homemade=650
-electric=300
-print(rent)
+# rent=3200
+# wifi=200
+# food=3500
+# homemade=650
+# electric=300
+# print(rent)
+#
+# total=rent+wifi+food+homemade+electric
+# print("My overall expence:",total)
 
-total=rent+wifi+food+homemade+electric
-print("My overall expence:",total)
+#===================
+
+# spend =3
+# donated= 4
+#
+# total_amount= spend + donated
+#
+# print(total_amount)
+
+#===========================
+#
+# items= 10
+# price= 2
+#
+# total_price= items * price
+#
+# print(total_price)
+
+#========================
+
+# items = 10
+# price = 2
+#
+# total_price = items * price
+
+# Below we print out three variables
+# print(total_price, items, price)
+
+
+#Simple Types: Integers, Strings, and Floats
+
+# x = 10
+# y = "10"
+# z = 10.1
+#
+# sum1 = x + y
+# sum2 = y + y
+#
+# print(sum1, sum2)
+# print(type(x), type(y), type(z))
+
+#list & Ranges ==========////////
+
+# student_grades= [9.1, 8.8, 7.5]
+
+# student_grades = [9, "hello", 1, 2, 4.33]  # Corrected code
+# print(student_grades)  # Print the list contents
+
+student_grades = list (range(0, 11))  # range
+
+print(student_grades)  # Print the range contents
 
